5766c0bf8ee430c4c103979f94b7a7235968da9e
ShoheiMitani <mitani49@gmail.com>
2025-06-08 15:29:31 +0900

    added class

--- #31/sample.rb
+++ #31/sample.rb
@@ -1,2 +1,12 @@
+class Author < ApplicationRecord
+  has_many :books
+end
+
+class Book < ApplicationRecord
+  belongs_to :author
+end
+
 author = Author.find(id: "Shoheyhey")
 Book.create!(author: author)
+
+author.books.create!(title: "Hello, World!")
